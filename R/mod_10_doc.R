a8595850e3173a58da41cc2c605058e590581642
Steven Ge <xijin.ge@sdstate.edu>
2022-02-16 16:53:18 -0600

    add documentation tab

--- R/mod_10_doc.R
+++ R/mod_10_doc.R
@@ -10,32 +10,14 @@
 mod_10_doc_ui <- function(id){
   ns <- NS(id)
   tabPanel(
-    "R",
-    sidebarLayout(
-      sidebarPanel(
-        h5(
-          "Test new tab",
-        ),
-        numericInput(
-          inputId = ns("sampleID"), 
-          label = h5("sample ID"), 
-          min = 1, 
-          max = 5, 
-          value = 1
-        ),
-      ),
-      mainPanel(
-        tabsetPanel(
-          id = ns("test_tabs"),
-          tabPanel(
-            tile = "Histogram",
-            p("test!"),
-            plotOutput(
-              outputId = ns("test1"),
-              width = "100%",
-              height = "500px"
-            )
-          )
+    "About",
+    fluidPage(
+      h3("Citation:"),
+      p("Ge, Son & Yao, iDEP: an integrated web application for differential 
+        expression and pathway analysis of RNA-Seq data, ", 
+        a("BMC Bioinformatics 19:1-24, 2018", 
+           href="https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-018-2486-6",
+           target="_blank"
         )
       )
     )
@@ -48,20 +30,12 @@ mod_10_doc_ui <- function(id){
 #' @noRd 
 mod_10_doc_server <- function(id, pre_process, idep_data, tab){
   moduleServer( id, function(input, output, session){
-    ns <- session$ns
-
-    output$test1 <- renderPlot({
-      req(!is.null(pre_process$data()))
-      doc_hist(
-        processed_data = pre_process$data(),
-        sampleID = input$sampleID )
-    })
- 
+    ns <- session$ns 
   })
 }
     
 ## To be copied in the UI
-# mod_09_doc_ui("09_doc_ui_1")
+# mod_09_doc_ui("10_doc_ui_1")
     
 ## To be copied in the server
-# mod_09_doc_server("09_doc_ui_1")
+# mod_09_doc_server("10_doc_ui_1")
