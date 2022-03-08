5e1902c473e66ff33ec92d5dea18d69aaeece4bc
bderenge <58050643+bderenge@users.noreply.github.com>
2022-03-08 13:54:21 -0600

    Fixed hardcoding of r-markdown report file location. Also commented out report generation section. This will be added back in  a separate pull request later.

--- R/mod_02_pre_process.R
+++ R/mod_02_pre_process.R
@@ -630,46 +630,51 @@ mod_02_pre_process_server <- function(id, load_data, tab) {
     )
 
     # Markdown report
-    output$report <- downloadHandler(
-      # For PDF output, change this to "report.pdf"
-      filename ="report.pdf",
-      content = function(file) {
-        # Copy the report file to a temporary directory before processing it, in
-        # case we don't have write permissions to the current working dir (which
-        # can happen when deployed).
-        #tempReport <- file.path(tempdir(), "test_workflow.Rmd")
-        #tempReport
-        #tempReport<-gsub("\\", "/",tempReport,fixed = TRUE)
-        markdown_location <-"C:/Users/bdere/Documents/GitHub/idepGolem/vignettes/test_workflow2.Rmd"
-        #file.copy(from=markdown_location,to= tempReport, overwrite = TRUE)
-
-        # Set up parameters to pass to Rmd document
-        params <- list(
-          loaded_data = load_data$converted_data(),
-          sample_info = load_data$sample_info(),
-          data_file_format = load_data$data_file_format(),
-          no_id_conversion = input$no_id_conversion,
-          min_counts = input$min_counts,
-          n_min_samples_count = input$n_min_samples_count,
-          counts_transform = input$counts_transform,
-          counts_log_start = input$counts_log_start,
-          log_transform_fpkm = input$log_transform_fpkm,
-          log_start_fpkm = input$log_start_fpkm,
-          missing_value = input$missing_value
-
-        )
-
-        # Knit the document, passing in the `params` list, and eval it in a
-        # child of the global environment (this isolates the code in the document
-        # from the code in this app).
-        rmarkdown::render(
-          input = markdown_location,
-          output_file = file,
-          params = params,
-          envir = new.env(parent = globalenv())
-        )
-      }
-    )
+    # output$report <- downloadHandler(
+    #   # For PDF output, change this to "report.pdf"
+    #   filename ="report.pdf",
+    #   content = function(file) {
+    #     # Copy the report file to a temporary directory before processing it, in
+    #     # case we don't have write permissions to the current working dir (which
+    #     # can happen when deployed).
+    #     #tempReport <- file.path(tempdir(), "test_workflow.Rmd")
+    #     #tempReport
+    #     #tempReport<-gsub("\\", "/",tempReport,fixed = TRUE)
+    #     
+    #     #This should retrieve the project location on your device:
+    #     #"C:/Users/bdere/Documents/GitHub/idepGolem"
+    #     wd <- getwd()
+    #     
+    #     markdown_location <-paste0(wd, "/vignettes/test_workflow2.Rmd")
+    #     #file.copy(from=markdown_location,to= tempReport, overwrite = TRUE)
+    # 
+    #     # Set up parameters to pass to Rmd document
+    #     params <- list(
+    #       loaded_data = load_data$converted_data(),
+    #       sample_info = load_data$sample_info(),
+    #       data_file_format = load_data$data_file_format(),
+    #       no_id_conversion = input$no_id_conversion,
+    #       min_counts = input$min_counts,
+    #       n_min_samples_count = input$n_min_samples_count,
+    #       counts_transform = input$counts_transform,
+    #       counts_log_start = input$counts_log_start,
+    #       log_transform_fpkm = input$log_transform_fpkm,
+    #       log_start_fpkm = input$log_start_fpkm,
+    #       missing_value = input$missing_value
+    # 
+    #     )
+    # 
+    #     # Knit the document, passing in the `params` list, and eval it in a
+    #     # child of the global environment (this isolates the code in the document
+    #     # from the code in this app).
+    #     rmarkdown::render(
+    #       input = markdown_location,
+    #       output_file = file,
+    #       params = params,
+    #       envir = new.env(parent = globalenv())
+    #     )
+    #   }
+    # )
 
     # Number of converted IDs ---------
     n_matched <- reactive({
