3cc224b3e6583256f249c1c137628ba9520a3f1e
Emma Spors <71563929+espors@users.noreply.github.com>
2022-09-07 17:15:18 -0500

    Change database function back

--- R/fct_database.R
+++ R/fct_database.R
@@ -18,8 +18,6 @@ if(nchar(DATAPATH) == 0) {
    DATAPATH = "../../data/data104b/"
 }
 
-DATAPATH <- "F:/IDEP_DATABASE/"
-
 #' connect_convert_db connects to the convertIDs.db and returns the
 #' objects.
 #'
