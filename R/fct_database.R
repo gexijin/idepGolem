28d03212aa335482af88b000ce5852625a4af266
Ben Derenge <58050643+bderenge@users.noreply.github.com>
2022-09-20 15:41:27 -0500

    Revert "commit"
    This reverts commit 938860d67546d543dc506f4a860071644a49308a.

--- R/fct_database.R
+++ R/fct_database.R
@@ -11,9 +11,11 @@
 NULL
 
 
-
-DATAPATH <- "D:/data104b_final/"
-
+# if environmental variable is not set, use relative path
+DATAPATH <- Sys.getenv("IDEP_DATABASE")[1]
+if (nchar(DATAPATH) == 0) {
+  DATAPATH <- "../../data/data104b/"
+}
 
 #' connect_convert_db connects to the convertIDs.db and returns the
 #' objects.
