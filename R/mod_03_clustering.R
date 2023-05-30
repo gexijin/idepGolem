6046732dbf87b3463413bad293308fe227e3d1f8
jennathor <82481199+jennathor@users.noreply.github.com>
2023-05-30 14:15:27 -0500

    added spinner to "how many clusters" in k-means

--- R/mod_03_clustering.R
+++ R/mod_03_clustering.R
@@ -818,6 +818,11 @@ mod_03_clustering_server <- function(id, pre_process, load_data, idep_data, tab)
     })
     # pop-up modal
     observeEvent(input$elbow_pop_up, {
+      shinybusy::show_modal_spinner(
+            spin = "orbit",
+            text = "Creating sub-heatmap",
+            color = "#000000"
+          )
       showModal(modalDialog(
         plotOutput(ns("k_clusters")),
         footer = NULL,
@@ -832,6 +837,7 @@ mod_03_clustering_server <- function(id, pre_process, load_data, idep_data, tab)
           )
         ),
       ))
+      shinybusy::remove_modal_spinner()
     })
 
     # Heatmap Download Data -----------
